4f5ebdd7e891dbd4dae8e6b98ede7ef9e053a8fe
ROJS <mskim829@naver.com>
2025-11-11 22:44:14 +0900

    Implement highlight as outline shader toggle

--- Interactables/interactable.gd
+++ Interactables/interactable.gd
@@ -2,18 +2,43 @@ class_name Interactable extends Area2D
 
 signal interacted(source:Node2D)
 @export var parent : Node2D
+@export var sprite : Sprite2D
+
+const OUTLINE_SHADER : Shader = preload("uid://bbqd5o24ax3fj")
 
 func _ready() -> void:
 	if not parent:
 		parent = get_parent() as Node2D
 	if parent.has_method("_on_interacted"):
 		interacted.connect(parent._on_interacted)
+	_find_sprite()
 
 func interact(_source:Node2D) -> void:
 	interacted.emit(_source)
 
 func highlight()->void:
-	parent.modulate.a = 0.3
+	if not sprite:
+		return
+	if not sprite.material is ShaderMaterial:
+		return
+	sprite.material.set_shader_parameter("enable", true)
 
 func unhighlight()->void:
-	parent.modulate.a = 1.0
+	if not sprite:
+		return
+	if not sprite.material is ShaderMaterial:
+		return
+	sprite.material.set_shader_parameter("enable", false)
+
+func _find_sprite()->bool:
+	if not parent:
+		return false
+	for child_node in parent.get_children():
+		if child_node is Sprite2D:
+			sprite = child_node
+			if not sprite.material:
+				sprite.material = ShaderMaterial.new()
+				sprite.material.shader = OUTLINE_SHADER
+			return true
+	return false
+	
